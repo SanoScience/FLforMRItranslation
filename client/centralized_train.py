f648df52cb1b8b68119b675be9225041f5cb6f8b
Jan Fiszer <j.fiszer@sanoscience.org>
2023-07-21 16:43:10 +0200

    delete totchsummary import

--- client/centralized_train.py
+++ client/centralized_train.py
@@ -3,7 +3,6 @@ import time
 
 import torch.nn as nn
 import torch.optim as optim
-from torchsummary import summary
 from torch.utils.data import DataLoader
 from torchmetrics.image import StructuralSimilarityIndexMeasure
 import torch
@@ -25,9 +24,9 @@ if __name__ == '__main__':
     train_dataset = datasets.MRIDatasetNumpySlices(train_directories)
     validation_dataset = datasets.MRIDatasetNumpySlices(validation_directories)
     # train_dataset = datasets.MRIDatasetNII(ROOT_DIR_TRAIN, transform=None)
-    trainloader = DataLoader(train_dataset, batch_size=config_train.BATCH_SIZE, shuffle=True, num_workers=config_train.NUM_WORKERS)
+    trainloader = DataLoader(train_dataset, batch_size=config_train.BATCH_SIZE, shuffle=True, num_workers=config_train.NUM_WORKERS, pin_memory=True)
     # trainloader = DataLoader(train_dataset, batch_size=config_train.BATCH_SIZE, shuffle=True)
-    valloader = DataLoader(validation_dataset, batch_size=config_train.BATCH_SIZE, shuffle=True, num_workers=config_train.NUM_WORKERS)
+    valloader = DataLoader(validation_dataset, batch_size=config_train.BATCH_SIZE, shuffle=True, num_workers=config_train.NUM_WORKERS, pin_memory=True)
     # valloader = DataLoader(validation_dataset, batch_size=config_train.BATCH_SIZE, shuffle=True)
 
     unet = models.UNet().to(config_train.DEVICE)
