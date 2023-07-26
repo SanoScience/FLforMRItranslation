25c7bebfb21c33bbe248469cbb0bb989cfd5c593
Jan Fiszer <j.fiszer@sanoscience.org>
2023-07-26 13:59:13 +0200

    dataset transpose IN and after data_dir t1 and t2 filepath

--- common/datasets.py
+++ common/datasets.py
@@ -23,23 +23,23 @@ class MRIDatasetNII(Dataset):
     MAX_SLICE_INDEX = 125
     STEP = 1
 
-    def __init__(self, data_dir: str, transform: Compose, n_patients=1, t1_to_t2=True):
+    def __init__(self, data_dir: str,  t1_filepath_from_data_dir, t2_filepath_from_data_dir, transform: Compose, transform_order=None, n_patients=1, t1_to_t2=True):
         self.data_dir = data_dir
         self.transform = transform
 
         if t1_to_t2:
-            images_filepaths, targets_filepaths = get_nii_filepaths(data_dir, n_patients)
+            images_filepaths, targets_filepaths = get_nii_filepaths(data_dir, t1_filepath_from_data_dir, t2_filepath_from_data_dir, n_patients)
         else:
-            targets_filepaths, images_filepaths = get_nii_filepaths(data_dir, n_patients)
+            targets_filepaths, images_filepaths = get_nii_filepaths(data_dir, t1_filepath_from_data_dir, t2_filepath_from_data_dir, n_patients)
 
         self.images, self.targets = [], []
 
         for img_path in images_filepaths:
-            slices, _, _ = load_nii_slices(img_path, self.MIN_SLICE_INDEX, self.MAX_SLICE_INDEX)
+            slices, _, _ = load_nii_slices(img_path, transform_order, self.MIN_SLICE_INDEX, self.MAX_SLICE_INDEX)
             self.images.extend(slices)
 
         for target_path in targets_filepaths:
-            slices, _, _ = load_nii_slices(target_path, self.MIN_SLICE_INDEX, self.MAX_SLICE_INDEX)
+            slices, _, _ = load_nii_slices(target_path, transform_order, self.MIN_SLICE_INDEX, self.MAX_SLICE_INDEX)
             self.targets.extend(slices)
 
     def __len__(self):
@@ -128,13 +128,7 @@ class MRIDatasetNumpySlices(Dataset):
 
 
 def get_optimal_slice_range(brain_slices, eps=1e-4, target_zero_ratio=0.9):
-    # in case of brain image being in wrong shape
-    # we want (n_slice, img_H, img_W)
-    # it changes from (img_H, img_W, n_slices) to desired length
-    if brain_slices.shape[0] == config_train.IMAGE_SIZE[0]:
-        brain_slices = np.transpose(brain_slices, (2, 0, 1))
-
-    zero_ratios = np.array([np.sum(brain_slice < eps) / (brain_slice.shape[0] * brain_slice.shape[0])
+    zero_ratios = np.array([np.sum(brain_slice < eps) / (brain_slice.shape[0] * brain_slice.shape[1])
                             for brain_slice in brain_slices])
     satisfying_given_ratio = np.where(zero_ratios < target_zero_ratio)[0]
 
@@ -144,11 +138,17 @@ def get_optimal_slice_range(brain_slices, eps=1e-4, target_zero_ratio=0.9):
     return upper_bound, lower_bound
 
 
-def load_nii_slices(filepath: str, min_slice_index=-1, max_slices_index=-1, index_step=1):
+def load_nii_slices(filepath: str, transpose_order, min_slice_index=-1, max_slices_index=-1, index_step=1):
     img = nib.load(filepath).get_fdata()
 
     if max_slices_index > img.shape[-1]:  # img.shape[-1] == total number of slices
         raise ValueError
+
+    # in case of brain image being in wrong shape
+    # we want (n_slice, img_H, img_W)
+    # it changes from (img_H, img_W, n_slices) to desired length
+    if transpose_order is not None:
+        img = np.transpose(img, transpose_order)
 
     if min_slice_index == -1 or max_slices_index == -1:
         min_slice_index, max_slices_index = get_optimal_slice_range(img)
@@ -158,20 +158,20 @@ def load_nii_slices(filepath: str, min_slice_index=-1, max_slices_index=-1, inde
     return [img[:, :, slice_index] for slice_index in range(min_slice_index, max_slices_index + 1, index_step)], min_slice_index, max_slices_index
 
 
-def get_nii_filepaths(data_dir, n_patients=-1):
+def get_nii_filepaths(data_dir, t1_filepath_from_data_dir, t2_filepath_from_data_dir, n_patients=-1):
+    # creating the t1 and t2 filepaths
+    t1_filepaths = []
+    t2_filepaths = []
+
     local_dirs = os.listdir(data_dir)
 
     # if not specified taking all patients
     if n_patients == -1:
         n_patients = len(local_dirs)
 
-    # creating the t1 and t2 filepaths
-    t1_filepaths = []
-    t2_filepaths = []
-
     for i in range(n_patients):
-        t1_filepaths.extend(sorted(glob(f"{data_dir}/{local_dirs[i]}/*t1.nii.gz")))
-        t2_filepaths.extend(sorted(glob(f"{data_dir}/{local_dirs[i]}/*t2.nii.gz")))
+        t1_filepaths.extend(sorted(glob(os.path.join(data_dir, local_dirs[i], t1_filepath_from_data_dir))))
+        t2_filepaths.extend(sorted(glob(os.path.join(data_dir, local_dirs[i], t2_filepath_from_data_dir))))
 
     print(f"Found {len(t1_filepaths)} t1 files and {len(t2_filepaths)} t2 files")
 
@@ -179,9 +179,10 @@ def get_nii_filepaths(data_dir, n_patients=-1):
 
 
 class TransformDataset:
-    def __init__(self, target_root_dir: str, origin_data_dir: str,):
+    def __init__(self, target_root_dir: str, origin_data_dir: str,transpose_order):
         self.target_root_dir = target_root_dir
         self.origin_data_dir = origin_data_dir
+        self.transpose_order = transpose_order
 
     def create_empty_dirs(self):
         # TODO: deal with already created directories, to prevent overwriting, IDEA- patient_index + n_files or maybe not needed
@@ -204,6 +205,8 @@ class TransformDataset:
         return t1_train_dir, t2_train_dir, t1_test_dir, t2_test_dir, t1_val_dir, t2_val_dir
 
     def create_train_val_test_sets(self,
+                                   t1_filepath_from_data_dir,
+                                   t2_filepath_from_data_dir,
                                    train_size=0.75,
                                    n_patients=-1,
                                    validation_size=0.1,
@@ -220,7 +223,10 @@ class TransformDataset:
               "\n", sep='\n')
 
         # loading the data
-        t1_filepaths, t2_filepaths = get_nii_filepaths(self.origin_data_dir, n_patients)
+        t1_filepaths, t2_filepaths = get_nii_filepaths(self.origin_data_dir,
+                                                       t1_filepath_from_data_dir,
+                                                       t1_filepath_from_data_dir,
+                                                       n_patients)
 
         # splitting filenames into train and test sets
         n_samples = len(t1_filepaths)
@@ -240,51 +246,12 @@ class TransformDataset:
         t2_val_paths = t2_filepaths[n_train_samples:n_val_samples + n_train_samples]
         t2_test_paths = t2_filepaths[n_val_samples + n_train_samples:]
 
-        # creating train set
         print("Creating train set...")
         self.create_set(t1_train_paths, t2_train_paths, t1_train_dir, t2_train_dir)
-        # for patient_id, (t1_path, t2_path) in enumerate(zip(t1_train_paths, t2_train_paths)):
-        #     print("Patient number ", patient_id, " in process...\n")
-        #
-        #     t1_slices, min_slice_index, max_slice_index = load_nii_slices(t1_path, MRIDatasetNumpySlices.MIN_SLICE_INDEX, MRIDatasetNumpySlices.MAX_SLICE_INDEX)
-        #     t2_slices, _, _ = load_nii_slices(t2_path, min_slice_index, max_slice_index)
-        #
-        #     for index, (t1_slice, t2_slice) in enumerate(zip(t1_slices, t2_slices)):
-        #         # getting the origin directory e.g. Brats18_2013_2_1
-        #         origin_dir = t1_path.split('/')[-2]
-        #         filename = f"{origin_dir}-patient{patient_id}-slice{index}{MRIDatasetNumpySlices.SLICES_FILE_FORMAT}"
-        #
-        #         # saving a t1 slice
-        #         t1_slice_path = os.path.join(t1_train_dir, filename)
-        #         np.save(t1_slice_path, t1_slice)
-        #
-        #         # saving a t2 slice
-        #         t2_slice_path = os.path.join(t2_train_dir, filename)
-        #         np.save(t2_slice_path, t2_slice)
-        #
-        #         print("Created pair of t1 and t2 slices: ", t1_slice_path, t2_slice_path)
 
-        # creating test set
         print("Creating test set...")
         self.create_set(t1_test_paths, t2_test_paths, t1_test_dir, t2_test_dir)
-        # for patient_id, (t1_path, t2_path) in enumerate(zip(t1_test_paths, t2_test_paths)):
-        #     print("Patient number ", patient_id, " in process...\n")
-        #     t1_slices, min_slice_index, max_slice_index = load_nii_slices(t1_path, MRIDatasetNumpySlices.MIN_SLICE_INDEX, MRIDatasetNumpySlices.MAX_SLICE_INDEX)
-        #     t2_slices, _, _ = load_nii_slices(t2_path, min_slice_index, max_slice_index)
-        #
-        #     for index, (t1_slice, t2_slice) in enumerate(zip(t1_slices, t2_slices)):
-        #         filename = f"patient{patient_id}-slice{index}{MRIDatasetNumpySlices.SLICES_FILE_FORMAT}"
-        #         # saving a t1 slice
-        #         t1_slice_path = os.path.join(t1_test_dir, filename)
-        #         np.save(t1_slice_path, t1_slice)
-        #
-        #         # saving a t2 slice
-        #         t2_slice_path = os.path.join(t2_test_dir, filename)
-        #         np.save(t2_slice_path, t2_slice)
-        #
-        #         print("Created a pair of t1 and t2 slices: ", t2_slice_path, t1_slice_path)
 
-        # creating validation set
         print("Creating validation set...")
         self.create_set(t1_val_paths, t2_val_paths, t1_val_dir, t2_val_dir)
 
@@ -292,13 +259,13 @@ class TransformDataset:
               f"from {n_train_samples} train, {n_val_samples} validation and {n_samples - n_train_samples - n_val_samples} "
               f"test 3D MRI images")
 
-    @staticmethod
-    def create_set(t1_paths, t2_paths, t1_dir, t2_dir):
+    def create_set(self, t1_paths, t2_paths, t1_dir, t2_dir):
         for patient_id, (t1_path, t2_path) in enumerate(zip(t1_paths, t2_paths)):
             print("Patient number ", patient_id, " in process...\n")
-            t1_slices, min_slice_index, max_slice_index = load_nii_slices(t1_path, MRIDatasetNumpySlices.MIN_SLICE_INDEX,
+            t1_slices, min_slice_index, max_slice_index = load_nii_slices(t1_path, self.transpose_order,
+                                                                          MRIDatasetNumpySlices.MIN_SLICE_INDEX,
                                                                           MRIDatasetNumpySlices.MAX_SLICE_INDEX)
-            t2_slices, _, _ = load_nii_slices(t2_path, min_slice_index, max_slice_index)
+            t2_slices, _, _ = load_nii_slices(t2_path, self.transpose_order, min_slice_index, max_slice_index)
 
             for index, (t1_slice, t2_slice) in enumerate(zip(t1_slices, t2_slices)):
                 filename = f"patient{patient_id}-slice{index}{MRIDatasetNumpySlices.SLICES_FILE_FORMAT}"
@@ -312,6 +279,7 @@ class TransformDataset:
 
                 print("Created pair of t1 and t2 slices: ", t1_slice_path, t2_slice_path)
 
+            print(f"T1 and T2 slice shape{t1_slice.shape} {t2_slice.shape}")
 
 # used when file format is jpeg
 def save_as_img(array, path):
--- transform_dataset.py
+++ transform_dataset.py
@@ -12,9 +12,10 @@ if __name__ == '__main__':
         n_patients = int(sys.argv[3])
     else:
         n_patients = -1
-    # current_data_dir = "C:\\Users\\JanFiszer\\data\\HGG\\"
-    # target_root_dir = "C:\\Users\\JanFiszer\\data\\hgg_transformed\\"
+    # current_data_dir = "C:\\Users\\JanFiszer\\data\\HCP_MGH"
+    # target_root_dir = "C:\\Users\\JanFiszer\\data\\hcp_mgh_first_test\\"
 
-    transformer = TransformDataset(target_root_dir, current_data_dir)
-    transformer.create_train_val_test_sets(n_patients=n_patients, seed=42)
+    transpose_order = (2, 1, 0)
+    transformer = TransformDataset(target_root_dir, current_data_dir, transpose_order)
+    transformer.create_train_val_test_sets("anat\\T1\\T1_bet.nii.gz", "anat\\t2\\T2_bet_reg.nii.gz", seed=42)
 
