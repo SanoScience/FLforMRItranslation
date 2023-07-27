56159663fff24430787e15870fb56bb63c6e6468
Jan Fiszer <j.fiszer@sanoscience.org>
2023-07-27 10:02:42 +0200

    transfer for ares

--- transform_dataset.py
+++ transform_dataset.py
@@ -6,14 +6,14 @@ from os import path
 
 
 if __name__ == '__main__':
-    # target_root_dir = sys.argv[1]
-    # current_data_dir = sys.argv[2]
+    target_root_dir = sys.argv[1]
+    current_data_dir = sys.argv[2]
     if len(sys.argv) > 3:
         n_patients = int(sys.argv[3])
     else:
         n_patients = -1
-    current_data_dir = "C:\\Users\\JanFiszer\\data\\HCP_Wu-Minn"
-    target_root_dir = "C:\\Users\\JanFiszer\\data\\hcp_wu_minn\\"
+    # current_data_dir = "C:\\Users\\JanFiszer\\data\\HCP_Wu-Minn"
+    # target_root_dir = "C:\\Users\\JanFiszer\\data\\hcp_wu_minn\\"
 
     # C:\Users\JanFiszer\data\HCP_Wu-Minn\114318_3T_Structural_preproc\114318\T1w
     transpose_order = (2, 0, 1)
