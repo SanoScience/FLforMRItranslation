c5cb457cd6e8750123784725d2d88c7b9b652497
Jan Fiszer <j.fiszer@sanoscience.org>
2023-07-27 16:08:35 +0200

    multipule train and val directories

--- centralized_train.py
+++ centralized_train.py
@@ -14,40 +14,37 @@ from client.utils import train
 if not config_train.LOCAL:
     os.chdir("repos/FLforMRItranslation")
 
-    train_directories = "/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/lgg/train"
-    validation_directories = "/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/lgg/validation"
+    train_directories = ["/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/lgg/train",
+                         "/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/hgg/train",
+                         "/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/hcp_mgh_masks/train",
+                         "/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/hcp_wu_minn"]
+    validation_directories = ["/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/lgg/validation",
+                              "/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/hgg/validation",
+                              "/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/hcp_mgh_masks/validation",
+                              "/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/hcp_wu_minn/validation"]
 else:
     train_directories = "C:\\Users\\JanFiszer\\data\\hgg_transformed\\validation"
     validation_directories = "C:\\Users\\JanFiszer\\data\\hgg_transformed\\validation"
     # ROOT_DIR_TRAIN = os.path.join(os.path.expanduser("~"), "data/HGG")
 
-
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        train_directories = sys.argv[1]
-        validation_directories = sys.argv[2]
+    train_dataset = MRIDatasetNumpySlices(train_directories)
+    validation_dataset = MRIDatasetNumpySlices(validation_directories)
 
-    train_dataset = MRIDatasetNumpySlices([train_directories])
-    validation_dataset = MRIDatasetNumpySlices([validation_directories])
+    num_workers = config_train.NUM_WORKERS
+    print(f"num_workers: {num_workers}")
 
-    if len(sys.argv) > 2:
-        num_workers = int(sys.argv[3])
-        print(f"num_workers: {num_workers}")
-
-        trainloader = DataLoader(train_dataset,
-                                 batch_size=config_train.BATCH_SIZE,
-                                 shuffle=True,
-                                 num_workers=num_workers,
-                                 pin_memory=True)
-        valloader = DataLoader(validation_dataset,
-                               batch_size=config_train.BATCH_SIZE,
-                               shuffle=True,
-                               num_workers=num_workers,
-                               pin_memory=True
-                               )
-    else:
-        trainloader = DataLoader(train_dataset, batch_size=config_train.BATCH_SIZE, shuffle=True)
-        valloader = DataLoader(validation_dataset, batch_size=config_train.BATCH_SIZE, shuffle=True)
+    trainloader = DataLoader(train_dataset,
+                             batch_size=config_train.BATCH_SIZE,
+                             shuffle=True,
+                             num_workers=num_workers,
+                             pin_memory=True)
+    valloader = DataLoader(validation_dataset,
+                           batch_size=config_train.BATCH_SIZE,
+                           shuffle=True,
+                           num_workers=num_workers,
+                           pin_memory=True
+                           )
 
     # train_dataset = MRIDatasetNumpySlices([ROOT_DIR_TRAIN])
     # validation_dataset = MRIDatasetNumpySlices([ROOT_DIR_VAL])
