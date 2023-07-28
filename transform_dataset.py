391ef034f6d884cde776510315cd1aa562867b96
Jan Fiszer <j.fiszer@sanoscience.org>
2023-07-28 10:03:26 +0200

    transfering sample of hgg

--- transform_dataset.py
+++ transform_dataset.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     # target_root_dir = "C:\\Users\\JanFiszer\\data\\mgh_masks_transformed"
 
     # C:\Users\JanFiszer\data\HCP_Wu-Minn\114318_3T_Structural_preproc\114318\T1w
-    transpose_order = (1, 2, 0)
+    transpose_order = (2, 0, 1)
     transformer = TransformDataset(target_root_dir, current_data_dir, transpose_order, target_zero_ratio=0.8)
-    transformer.create_train_val_test_sets("T1/T1_bet.nii.gz", "T2/T2_bet_reg.nii.gz", n_patients=n_patients, seed=42)
+    transformer.create_train_val_test_sets("*t1.nii.gz", "*t2.nii.gz", n_patients=n_patients, seed=42)
 
