27cd7808a2b6299c730a16271f30318d0d4101da
Jan Fiszer <j.fiszer@sanoscience.org>
2023-07-31 17:17:03 +0200

    data loaders as client arguments

--- client_train.py
+++ client_train.py
@@ -12,9 +12,13 @@ from client.utils import train, test, load_data
 # TODO: client __init__ containting the model
 
 class TranslationClient(fl.client.NumPyClient):
-    def __init__(self, client_id, model: models.UNet):
+    def __init__(self, client_id, model: models.UNet, train_loader, test_loader, val_loader):
         self.client_id = client_id
         self.model = model
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.val_loader = val_loader
+
     def get_parameters(self, config):
         return [val.cpu().numpy() for val in self.model.state_dict().values()]
 
@@ -27,17 +31,17 @@ class TranslationClient(fl.client.NumPyClient):
         self.set_parameters(parameters)
         if config_train.LOCAL:
             # to optimize
-            train(self.model, val_loader, optimizer, epochs=config_train.N_EPOCHS_CLIENT)
+            train(self.model, self.val_loader, optimizer, epochs=config_train.N_EPOCHS_CLIENT)
         else:
-            train(self.model, train_loader, optimizer, validationloader=val_loader, epochs=config_train.N_EPOCHS_CLIENT)
+            train(self.model, self.train_loader, optimizer, validationloader=self.val_loader, epochs=config_train.N_EPOCHS_CLIENT)
 
         return self.get_parameters(config={}), len(train_loader.dataset), {}
 
     def evaluate(self, parameters, config):
         # TODO: valset instead of test
         self.set_parameters(parameters)
-        loss, ssim = test(self.model, test_loader)
-        return loss, len(test_loader.dataset), {"ssim": ssim}
+        loss, ssim = test(self.model, self.test_loader)
+        return loss, len(self.test_loader.dataset), {"ssim": ssim}
 
 
 if __name__ == "__main__":
@@ -50,7 +54,7 @@ if __name__ == "__main__":
     client_id = sys.argv[2]
     server_node = sys.argv[3]
     # data_dir = os.path.join(config_train.DATA_ROOT_DIR, "small_hgg")
-    train_loader, test_loader, val_loader = load_data(data_dir, with_num_workers=True)
+    trainloader, testloader, valloader = load_data(data_dir, with_num_workers=True)
 
     # Model
     unet = models.UNet().to(config_train.DEVICE)
@@ -60,5 +64,5 @@ if __name__ == "__main__":
 
     fl.client.start_numpy_client(
         server_address=server_address,
-        client=TranslationClient(client_id=client_id, model=unet)
+        client=TranslationClient(client_id, unet, trainloader, testloader, valloader)
     )
