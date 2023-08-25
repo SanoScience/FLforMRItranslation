c6bd1f12588de797a6280ac03e6f57e3873db85c
Jan Fiszer <j.fiszer@sanoscience.org>
2023-08-25 11:29:48 +0200

    old unet testing started

--- centralized_train.py
+++ centralized_train.py
@@ -19,8 +19,8 @@ if not config_train.LOCAL:
                             #   "/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/hcp_mgh_masks/validation",
                             #   "/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/hcp_wu_minn/validation"]
 else:
-    train_directories = "C:\\Users\\JanFiszer\\data\\hgg_transformed\\validation"
-    validation_directories = "C:\\Users\\JanFiszer\\data\\hgg_transformed\\validation"
+    train_directories = ["C:\\Users\\JanFiszer\\data\\hgg_transformed\\validation"]
+    validation_directories = ["C:\\Users\\JanFiszer\\data\\hgg_transformed\\validation"]
     # ROOT_DIR_TRAIN = os.path.join(os.path.expanduser("~"), "data/HGG")
 
 if __name__ == '__main__':
@@ -33,12 +33,12 @@ if __name__ == '__main__':
     trainloader = DataLoader(train_dataset,
                              batch_size=config_train.BATCH_SIZE,
                              shuffle=True,
-                             num_workers=num_workers,
+                             num_workers=config_train.NUM_WORKERS,
                              pin_memory=True)
     valloader = DataLoader(validation_dataset,
                            batch_size=config_train.BATCH_SIZE,
                            shuffle=True,
-                           num_workers=num_workers,
+                           num_workers=config_train.NUM_WORKERS,
                            pin_memory=True)
 
     # train_dataset = MRIDatasetNumpySlices([ROOT_DIR_TRAIN])
@@ -46,17 +46,25 @@ if __name__ == '__main__':
     # trainloader = DataLoader(train_dataset, batch_size=config_train.BATCH_SIZE, shuffle=True)
     # valloader = DataLoader(validation_dataset, batch_size=config_train.BATCH_SIZE, shuffle=True)
 
-    unet = UNet().to(config_train.DEVICE)
-    optimizer = optim.Adam(unet.parameters(), lr=config_train.LEARNING_RATE)
+    old_unet = OldUNet().to(config_train.DEVICE)
+    optimizer = optim.Adam(old_unet.parameters(), lr=config_train.LEARNING_RATE)
     criterion = loss_functions.DssimMse()
-    model_filename = f"model.pth"
 
-    unet.perform_train(trainloader,
-                       optimizer,
-                       criterion,
-                       validationloader=valloader,
-                       epochs=config_train.N_EPOCHS_CENTRALIZED,
-                       model_dir=config_train.CENTRALIZED_DIR,
-                       filename=model_filename,
-                       plots_dir="predictions", 
-                       history_filename="history.pkl")
+    train(old_unet, trainloader, valloader, optimizer, criterion, epochs=config_train.N_EPOCHS_CENTRALIZED,
+          filename="model.pth", history_filename="history.pkl", plots_dir="predictions")
+
+
+    # unet = UNet().to(config_train.DEVICE)
+    # optimizer = optim.Adam(unet.parameters(), lr=config_train.LEARNING_RATE)
+    # criterion = loss_functions.DssimMse()
+    # model_filename = f"model.pth"
+    #
+    # unet.perform_train(trainloader,
+    #                    optimizer,
+    #                    criterion,
+    #                    validationloader=valloader,
+    #                    epochs=config_train.N_EPOCHS_CENTRALIZED,
+    #                    model_dir=config_train.CENTRALIZED_DIR,
+    #                    filename=model_filename,
+    #                    plots_dir="predictions",
+    #                    history_filename="history.pkl")
--- src/models.py
+++ src/models.py
@@ -322,6 +322,116 @@ class Up(nn.Module):
         return self.conv(x)
 
 
+class OldUNet(nn.Module):
+    def __init__(self, bilinear=False):
+        super(OldUNet, self).__init__()
+        self.bilinear = bilinear
+
+        self.inc = (OldDoubleConv(1, 64))
+        self.down1 = (OldDown(64, 128))
+        self.down2 = (OldDown(128, 256))
+        self.down3 = (OldDown(256, 512))
+        factor = 2 if bilinear else 1
+        self.down4 = (OldDown(512, 1024 // factor))
+        self.up1 = (OldUp(1024, 512 // factor, bilinear))
+        self.up2 = (OldUp(512, 256 // factor, bilinear))
+        self.up3 = (OldUp(256, 128 // factor, bilinear))
+        self.up4 = (OldUp(128, 64, bilinear))
+        self.outc = (OutConv(64, 1))
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+
+    def save(self, dir_name: str, filename=None):
+        if filename is None:
+            filename = "model"
+
+        if not isinstance(dir_name, str):
+            raise TypeError(f"Given directory name {dir_name} has wrong type: {type(dir_name)}.")
+
+        fop.try_create_dir(dir_name)
+
+        if not filename.endswith(".pth"):
+            filename += ".pth"
+
+        filepath = f"{dir_name}/{filename}"
+        torch.save(self.state_dict(), filepath)
+
+        print("Model saved to: ", filepath)
+
+
+class OldDoubleConv(nn.Module):
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class OldDown(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            OldDoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class OldUp(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = OldDoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = OldDoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(OutConv, self).__init__()
@@ -329,3 +439,156 @@ class OutConv(nn.Module):
 
     def forward(self, x):
         return torch.sigmoid(self.conv(x))
+
+
+def train(model,
+          trainloader,
+          validationloader,
+          optimizer,
+          criterion,
+          epochs,
+          filename=None,
+          history_filename="history.pkl",
+          plots_dir=None):
+    # TODO: transform from config to local vars
+
+    print(f"Training \non device: {config_train.DEVICE} \nwith loss: {criterion})...\n")
+
+    model_dir = config_train.TRAINED_MODEL_SERVER_DIR
+    fop.try_create_dir(model_dir)
+    print(f"Created directory {model_dir}")
+
+    n_batches = len(trainloader)
+
+    if n_batches < config_train.BATCH_PRINT_FREQ:
+        loss_print_freq = n_batches - 2  # tbh not sure if this -2 is needed
+    else:
+        loss_print_freq = config_train.BATCH_PRINT_FREQ
+
+    train_losses = []
+    train_ssims = []
+    val_losses = []
+    val_ssims = []
+
+    n_train_steps = len(trainloader.dataset) // config_train.BATCH_SIZE
+    n_val_steps = len(validationloader.dataset) // config_train.BATCH_SIZE
+
+    if plots_dir is not None:
+        plots_path = path.join(model_dir, plots_dir)
+        fop.try_create_dir(plots_path)
+
+    for epoch in range(epochs):
+        print("EPOCH: ", epoch + 1)
+
+        running_loss, total_ssim = 0.0, 0.0
+        epoch_loss, epoch_ssim = 0.0, 0.0
+
+        start = time.time()
+
+        for index, data in enumerate(trainloader):
+            images, targets = data
+            images = images.to(config_train.DEVICE)
+            targets = targets.to(config_train.DEVICE)
+
+            optimizer.zero_grad()
+
+            predictions = model(images)
+            loss = criterion(predictions, targets)
+            loss.backward()
+
+            optimizer.step()
+
+            # predictions_double = predictions.double()
+            # targets_double = targets.double()
+
+            # print(f"Predictions shape: {predictions_double.shape} type: {predictions_double.type()}")
+            # print(f"Targets shape: {targets_double.shape} type: {targets_double.type()}")
+
+            ssim_value = ssim(predictions, targets)
+
+            running_loss += loss.item()
+            total_ssim += ssim_value.item()
+
+            epoch_loss += loss.item()
+            epoch_ssim += ssim_value.item()
+
+            if index % loss_print_freq == loss_print_freq - 1:
+
+                print(f'batch {(index + 1)} out of {n_batches}\t'
+                      f'loss: {running_loss / loss_print_freq:.3f} '
+                      f'ssim {total_ssim / loss_print_freq:.3f}')
+
+                running_loss = 0.0
+                total_ssim = 0.0
+
+        epoch_loss /= n_train_steps
+        epoch_ssim /= n_train_steps
+        print(f"\nTime exceeded: {time.time() - start:.1f} "
+              f"epoch loss: {epoch_loss:.3f} ssim: {epoch_ssim:.3f}")
+        print()
+
+        train_ssims.append(epoch_ssim)
+        train_losses.append(epoch_loss)
+
+        print("Validation set in progress...")
+        val_loss = 0.0
+        val_ssim = 0.0
+        with torch.no_grad():
+            for images_cpu, targets_cpu in validationloader:
+                images = images_cpu.to(config_train.DEVICE)
+                targets = targets_cpu.to(config_train.DEVICE)
+
+                predictions = model(images)
+                loss = config_train.CRITERION(predictions, targets)
+
+                val_loss += loss.item()
+                val_ssim += ssim(predictions, targets).item()
+
+        val_loss /= n_val_steps
+        val_ssim /= n_val_steps
+        print(f"For validation set: val_loss: {val_loss:.3f} "
+              f"val_ssim: {val_ssim:.3f}")
+
+        val_ssims.append(val_ssim)
+        val_losses.append(val_loss)
+
+        if plots_dir is not None:
+            filepath = path.join(model_dir, plots_dir, f"ep{epoch}.jpg")
+            # maybe cast to cpu ?? still dunno if needed
+            visualization.plot_batch([images, targets, predictions.to('cpu').detach()], filepath=filepath)
+
+    print("\nEnd of this round.")
+
+    history = {"loss": train_losses, "ssim": train_ssims, "val_loss": val_losses, "val_ssim": val_ssims}
+
+    # saving
+    if history_filename is not None:
+        with open(path.join(model_dir, history_filename), 'wb') as file:
+            pickle.dump(history, file)
+
+    if filename is not None:
+        model.save(model_dir, filename)
+
+    return history
+
+
+def test(model, testloader, criterion):
+    print(f"Testing \non device: {config_train.DEVICE} \nwith loss: {criterion})...\n")
+    n_steps = 0
+
+    total_loss = 0.0
+    total_ssim = 0.0
+    with torch.no_grad():
+        for images_cpu, targets_cpu in testloader:
+            images = images_cpu.to(config_train.DEVICE)
+            targets = targets_cpu.to(config_train.DEVICE)
+
+            predictions = model(images)
+            loss = criterion(predictions, targets)
+
+            total_loss += loss.item()
+            total_ssim += ssim(predictions, targets).item()
+
+            n_steps += 1
+
+    return total_loss / n_steps, total_ssim / n_steps
