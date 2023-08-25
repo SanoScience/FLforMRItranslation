81107321c8ed1dd9507279199f04c1d74124c471
Jan Fiszer <j.fiszer@sanoscience.org>
2023-08-25 17:40:30 +0200

    train and test as methods Unet4

--- src/models.py
+++ src/models.py
@@ -396,6 +396,155 @@ class OldDoubleConv(nn.Module):
 
         return x
 
+    def perform_train(model,
+              trainloader,
+              validationloader,
+              optimizer,
+              criterion,
+              epochs,
+              filename=None,
+              history_filename="history.pkl",
+              plots_dir=None):
+        # TODO: transform from config to local vars
+
+        print(f"Training \non device: {config_train.DEVICE} \nwith loss: {criterion})...\n")
+
+        model_dir = config_train.CENTRALIZED_DIR
+        fop.try_create_dir(model_dir)
+        print(f"Created directory {model_dir}")
+
+        n_batches = len(trainloader)
+
+        if n_batches < config_train.BATCH_PRINT_FREQ:
+            loss_print_freq = n_batches - 2  # tbh not sure if this -2 is needed
+        else:
+            loss_print_freq = config_train.BATCH_PRINT_FREQ
+
+        train_losses = []
+        train_ssims = []
+        val_losses = []
+        val_ssims = []
+
+        n_train_steps = len(trainloader.dataset) // config_train.BATCH_SIZE
+        n_val_steps = len(validationloader.dataset) // config_train.BATCH_SIZE
+
+        if plots_dir is not None:
+            plots_path = path.join(model_dir, plots_dir)
+            fop.try_create_dir(plots_path)
+
+        for epoch in range(epochs):
+            print("EPOCH: ", epoch + 1)
+
+            running_loss, total_ssim = 0.0, 0.0
+            epoch_loss, epoch_ssim = 0.0, 0.0
+
+            start = time.time()
+
+            for index, data in enumerate(trainloader):
+                images, targets = data
+                images = images.to(config_train.DEVICE)
+                targets = targets.to(config_train.DEVICE)
+
+                optimizer.zero_grad()
+
+                predictions = model(images)
+                loss = criterion(predictions, targets)
+                loss.backward()
+
+                optimizer.step()
+
+                # predictions_double = predictions.double()
+                # targets_double = targets.double()
+
+                # print(f"Predictions shape: {predictions_double.shape} type: {predictions_double.type()}")
+                # print(f"Targets shape: {targets_double.shape} type: {targets_double.type()}")
+                ssim_value = ssim(predictions, targets)
+
+                running_loss += loss.item()
+                total_ssim += ssim_value.item()
+
+                epoch_loss += loss.item()
+                epoch_ssim += ssim_value.item()
+
+                if index % loss_print_freq == loss_print_freq - 1:
+
+                    print(f'batch {(index + 1)} out of {n_batches}\t'
+                          f'loss: {running_loss / loss_print_freq:.3f} '
+                          f'ssim {total_ssim / loss_print_freq:.3f}')
+
+                    running_loss = 0.0
+                    total_ssim = 0.0
+
+            epoch_loss /= n_train_steps
+            epoch_ssim /= n_train_steps
+            print(f"\nTime exceeded: {time.time() - start:.1f} "
+                  f"epoch loss: {epoch_loss:.3f} ssim: {epoch_ssim:.3f}")
+            print()
+
+            train_ssims.append(epoch_ssim)
+            train_losses.append(epoch_loss)
+
+            print("Validation set in progress...")
+            val_loss = 0.0
+            val_ssim = 0.0
+            with torch.no_grad():
+                for images_cpu, targets_cpu in validationloader:
+                    images = images_cpu.to(config_train.DEVICE)
+                    targets = targets_cpu.to(config_train.DEVICE)
+
+                    predictions = model(images)
+                    loss = criterion(predictions, targets)
+
+                    val_loss += loss.item()
+                    val_ssim += ssim(predictions, targets).item()
+
+            val_loss /= n_val_steps
+            val_ssim /= n_val_steps
+            print(f"For validation set: val_loss: {val_loss:.3f} "
+                  f"val_ssim: {val_ssim:.3f}")
+
+            val_ssims.append(val_ssim)
+            val_losses.append(val_loss)
+
+            if plots_dir is not None:
+                filepath = path.join(model_dir, plots_dir, f"ep{epoch}.jpg")
+                # maybe cast to cpu ?? still dunno if needed
+                visualization.plot_batch([images.to('cpu'), targets.to('cpu'), predictions.to('cpu').detach()], filepath=filepath)
+
+        print("\nEnd of this round.")
+
+        history = {"loss": train_losses, "ssim": train_ssims, "val_loss": val_losses, "val_ssim": val_ssims}
+
+        # saving
+        if history_filename is not None:
+            with open(path.join(model_dir, history_filename), 'wb') as file:
+                pickle.dump(history, file)
+
+        if filename is not None:
+            model.save(model_dir, filename)
+
+        return history
+
+    def test(model, testloader, criterion):
+        print(f"Testing \non device: {config_train.DEVICE} \nwith loss: {criterion})...\n")
+        n_steps = 0
+
+        total_loss = 0.0
+        total_ssim = 0.0
+        with torch.no_grad():
+            for images_cpu, targets_cpu in testloader:
+                images = images_cpu.to(config_train.DEVICE)
+                targets = targets_cpu.to(config_train.DEVICE)
+
+                predictions = model(images)
+                loss = criterion(predictions, targets)
+
+                total_loss += loss.item()
+                total_ssim += ssim(predictions, targets).item()
+
+                n_steps += 1
+
+        return total_loss / n_steps, total_ssim / n_steps
 
 class OldDown(nn.Module):
     """Downscaling with maxpool then double conv"""
@@ -449,154 +598,3 @@ class OutConv(nn.Module):
         return torch.sigmoid(self.conv(x))
 
 
-def train(model,
-          trainloader,
-          validationloader,
-          optimizer,
-          criterion,
-          epochs,
-          filename=None,
-          history_filename="history.pkl",
-          plots_dir=None):
-    # TODO: transform from config to local vars
-
-    print(f"Training \non device: {config_train.DEVICE} \nwith loss: {criterion})...\n")
-
-    model_dir = config_train.CENTRALIZED_DIR
-    fop.try_create_dir(model_dir)
-    print(f"Created directory {model_dir}")
-
-    n_batches = len(trainloader)
-
-    if n_batches < config_train.BATCH_PRINT_FREQ:
-        loss_print_freq = n_batches - 2  # tbh not sure if this -2 is needed
-    else:
-        loss_print_freq = config_train.BATCH_PRINT_FREQ
-
-    train_losses = []
-    train_ssims = []
-    val_losses = []
-    val_ssims = []
-
-    n_train_steps = len(trainloader.dataset) // config_train.BATCH_SIZE
-    n_val_steps = len(validationloader.dataset) // config_train.BATCH_SIZE
-
-    if plots_dir is not None:
-        plots_path = path.join(model_dir, plots_dir)
-        fop.try_create_dir(plots_path)
-
-    for epoch in range(epochs):
-        print("EPOCH: ", epoch + 1)
-
-        running_loss, total_ssim = 0.0, 0.0
-        epoch_loss, epoch_ssim = 0.0, 0.0
-
-        start = time.time()
-
-        for index, data in enumerate(trainloader):
-            images, targets = data
-            images = images.to(config_train.DEVICE)
-            targets = targets.to(config_train.DEVICE)
-
-            optimizer.zero_grad()
-
-            predictions = model(images)
-            loss = criterion(predictions, targets)
-            loss.backward()
-
-            optimizer.step()
-
-            # predictions_double = predictions.double()
-            # targets_double = targets.double()
-
-            # print(f"Predictions shape: {predictions_double.shape} type: {predictions_double.type()}")
-            # print(f"Targets shape: {targets_double.shape} type: {targets_double.type()}")
-
-            ssim_value = ssim(predictions, targets)
-
-            running_loss += loss.item()
-            total_ssim += ssim_value.item()
-
-            epoch_loss += loss.item()
-            epoch_ssim += ssim_value.item()
-
-            if index % loss_print_freq == loss_print_freq - 1:
-
-                print(f'batch {(index + 1)} out of {n_batches}\t'
-                      f'loss: {running_loss / loss_print_freq:.3f} '
-                      f'ssim {total_ssim / loss_print_freq:.3f}')
-
-                running_loss = 0.0
-                total_ssim = 0.0
-
-        epoch_loss /= n_train_steps
-        epoch_ssim /= n_train_steps
-        print(f"\nTime exceeded: {time.time() - start:.1f} "
-              f"epoch loss: {epoch_loss:.3f} ssim: {epoch_ssim:.3f}")
-        print()
-
-        train_ssims.append(epoch_ssim)
-        train_losses.append(epoch_loss)
-
-        print("Validation set in progress...")
-        val_loss = 0.0
-        val_ssim = 0.0
-        with torch.no_grad():
-            for images_cpu, targets_cpu in validationloader:
-                images = images_cpu.to(config_train.DEVICE)
-                targets = targets_cpu.to(config_train.DEVICE)
-
-                predictions = model(images)
-                loss = criterion(predictions, targets)
-
-                val_loss += loss.item()
-                val_ssim += ssim(predictions, targets).item()
-
-        val_loss /= n_val_steps
-        val_ssim /= n_val_steps
-        print(f"For validation set: val_loss: {val_loss:.3f} "
-              f"val_ssim: {val_ssim:.3f}")
-
-        val_ssims.append(val_ssim)
-        val_losses.append(val_loss)
-
-        if plots_dir is not None:
-            filepath = path.join(model_dir, plots_dir, f"ep{epoch}.jpg")
-            # maybe cast to cpu ?? still dunno if needed
-            visualization.plot_batch([images.to('cpu'), targets.to('cpu'), predictions.to('cpu').detach()], filepath=filepath)
-
-    print("\nEnd of this round.")
-
-    history = {"loss": train_losses, "ssim": train_ssims, "val_loss": val_losses, "val_ssim": val_ssims}
-
-    # saving
-    if history_filename is not None:
-        with open(path.join(model_dir, history_filename), 'wb') as file:
-            pickle.dump(history, file)
-
-    if filename is not None:
-        model.save(model_dir, filename)
-
-    return history
-
-
-def test(model, testloader, criterion):
-    print(f"Testing \non device: {config_train.DEVICE} \nwith loss: {criterion})...\n")
-    n_steps = 0
-
-    total_loss = 0.0
-    total_ssim = 0.0
-    with torch.no_grad():
-        for images_cpu, targets_cpu in testloader:
-            images = images_cpu.to(config_train.DEVICE)
-            targets = targets_cpu.to(config_train.DEVICE)
-
-            predictions = model(images)
-            loss = criterion(predictions, targets)
-
-            total_loss += loss.item()
-            total_ssim += ssim(predictions, targets).item()
-
-            n_steps += 1
-
-    return total_loss / n_steps, total_ssim / n_steps
