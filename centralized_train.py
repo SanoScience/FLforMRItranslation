ec310491527e0f10fe6dfc1198e983bc0a5f8f7c
Jan Fiszer <plgfiszer@login01.athena.cyfronet.pl>
2023-09-20 10:09:13 +0200

    logs improvement

--- centralized_train.py
+++ centralized_train.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     validation_dataset = MRIDatasetNumpySlices(validation_directories)
 
     num_workers = config_train.NUM_WORKERS
-    print(f"num_workers: {num_workers}")
+    print(f"Training with {num_workers} num_workers.")
 
     trainloader = DataLoader(train_dataset,
                              batch_size=config_train.BATCH_SIZE,
@@ -41,20 +41,3 @@ if __name__ == '__main__':
                            model_dir=config_train.CENTRALIZED_DIR,
                            history_filename="history.pkl",
                            plots_dir="predictions")
-
-
-
-    # unet = UNet().to(config_train.DEVICE)
-    # optimizer = optim.Adam(unet.parameters(), lr=config_train.LEARNING_RATE)
-    # criterion = loss_functions.DssimMse()
-    # model_filename = f"model.pth"
-    
-    # unet.perform_train(trainloader,
-    #                    optimizer,
-    #                    criterion,
-    #                    validationloader=valloader,
-    #                    epochs=config_train.N_EPOCHS_CENTRALIZED,
-    #                    model_dir=config_train.CENTRALIZED_DIR,
-    #                    filename=model_filename,
-    #                    plots_dir="predictions",
-    #                    history_filename="history.pkl")
