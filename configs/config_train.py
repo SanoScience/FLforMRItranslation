4c56d33f6d6936690b87ec7c662d9cdbb6c4b796
Jan Fiszer <plgfiszer@login01.athena.cyfronet.pl>
2023-09-25 13:00:30 +0200

    a

--- configs/config_train.py
+++ configs/config_train.py
@@ -5,12 +5,12 @@ from configs.enums import *
 
 LOCAL = False
 
-N_EPOCHS_CENTRALIZED = 32
+N_EPOCHS_CENTRALIZED = 50
 
 # training parameters
-CLIENT_TYPE = ClientTypes.FED_AVG
+CLIENT_TYPE = ClientTypes.FED_PROX
 LOSS_TYPE = LossFunctions.MSE_DSSIM
-AGGREGATION_METHOD = AggregationMethods.FED_AVGM
+AGGREGATION_METHOD = AggregationMethods.FED_AVG
 
 
 # model parameters
@@ -33,21 +33,21 @@ PORT = "8084"
 
 # federated learning parameters
 N_ROUNDS = 32
-SAVING_FREQUENCY = 1
+SAVING_FREQUENCY = 2
 MIN_FIT_CLIENTS = 5
 FRACTION_FIT = 1.0
 MIN_AVAILABLE_CLIENTS = 5
 # FedOpt
 TAU = 1e-3
 # FedProx
-PROXIMAL_MU = 0.5
+PROXIMAL_MU = 0.01
 STRAGGLERS = 0.2
 # FedAvgM
 MOMENTUM = 0.9
 
 # directories
 if LOCAL:
-    DATA_ROOT_DIR = os.path.join(os.path.expanduser("~"), "data")
+    DATA_ROOT_DIR = path.join(path.expanduser("~"), "data")
 else:
     DATA_ROOT_DIR = "/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL"
 
@@ -58,6 +58,6 @@ EVAL_DATA_DIRS = [path.join(DATA_ROOT_DIR, "lgg_26", "test"),
                   path.join(DATA_ROOT_DIR, "oasis_26", "test")]
 
 now = datetime.datetime.now()
-CENTRALIZED_DIR = f"{DATA_ROOT_DIR}/trained_models/model-centralized-{LOSS_TYPE.name}-ep{N_EPOCHS_CENTRALIZED}-lr{LEARNING_RATE}-{NORMALIZATION.name}-{now.date()}-{now.hour}h"
+CENTRALIZED_DIR = f"{DATA_ROOT_DIR}/trained_models/model-mgh-centralized-{LOSS_TYPE.name}-ep{N_EPOCHS_CENTRALIZED}-lr{LEARNING_RATE}-{NORMALIZATION.name}-{now.date()}-{now.hour}h"
 _REPRESENTATIVE_WORD = CLIENT_TYPE if CLIENT_TYPE == ClientTypes.FED_BN else AGGREGATION_METHOD
 TRAINED_MODEL_SERVER_DIR = f"{DATA_ROOT_DIR}/trained_models/model-{_REPRESENTATIVE_WORD.name}-{LOSS_TYPE.name}-lr{LEARNING_RATE}-rd{N_ROUNDS}-ep{N_EPOCHS_CLIENT}-{NORMALIZATION.name}-{now.date()}-{now.hour}h"
