f39fda4e24355b5eb80f40a484f02d43f8e5a2ac
Jan Fiszer <plgfiszer@login01.athena.cyfronet.pl>
2023-10-16 14:25:13 +0200

    athena dataset test and centralized train on top

--- dataset-test.py
+++ dataset-test.py
@@ -1,9 +1,6 @@
-import os.path
 import sys
 
 from os import path
-
-import numpy as np
 import torch
 
 from src.visualization import plot_batch
@@ -21,29 +18,23 @@ if __name__ == '__main__':
     dataset = MRIDatasetNumpySlices([data_dir_ares])
 
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True)
-    images, targets = next(iter(dataloader))
 
-    # for images, targets in dataloader:
-    #     for image, target in zip(images, targets):
-    #         if first_iteration:
-    #             image_shape = image.shape
-    #             target_shape = target.shape
-    #             first_iteration = False
-    #         else:
-    #             if image.shape != image_shape:
-    #                 print(f"WARNING: The shapes are different", image_shape, "!=", image.shape)
-    #             elif target.shape != target_shape:
-    #                 print(f"WARNING: The shapes are different", target_shape, "!=", target.shape)
     first_iteration = True
-    for numpy_img_path in os.path.join(data_dir_ares, "t1"):
-        numpy_img = np.load(path.join(numpy_img_path, numpy_img_path))
-        if first_iteration:
-            image_shape = numpy_img.shape
-            first_iteration = False
-        else:
-            if numpy_img.shape != image_shape:
-                print(f"WARNING: The shapes are different", image_shape, "!=", numpy_img.shape)
+    for images, targets in dataloader:
+        for image, target in zip(images, targets):
+            if first_iteration:
+                image_shape = image.shape
+                target_shape = target.shape
+                first_iteration = False
+            else:
+                if image.shape != image_shape:
+                    print(f"WARNING: The shapes are different", image_shape, "!=", image.shape)
+                elif target.shape != target_shape:
+                    print(f"WARNING: The shapes are different", target_shape, "!=", target.shape)
+                else:
+                    print("good")
 
     plot_batch([images, targets], filepath="plot_maybe_bad.jpg", show=False)
+
 
 
