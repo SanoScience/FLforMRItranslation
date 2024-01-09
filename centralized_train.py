8a8b30950399f1a985495fc855bf92b57de0926c
Jan Fiszer <plgfiszer@login01.athena.cyfronet.pl>
2024-01-09 12:20:05 +0100

    default train and validation set for training; config_train.T1_TO_T2

--- centralized_train.py
+++ centralized_train.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import torch.optim as optim
 from torch.nn import MSELoss
 
@@ -13,11 +14,17 @@ if __name__ == '__main__':
         train_directories = ["C:\\Users\\JanFiszer\\data\\mega_small_hgg\\train"]
         validation_directories = ["C:\\Users\\JanFiszer\\data\\mega_small_hgg\\train"]
     else:
-        train_directories = ["/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/hgg/train"]
-        validation_directories = ["/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/hgg/validation"]
+        if len(sys.argv) > 1:
+            data_dir = sys.argv[1]
+            train_directories = [os.path.join(data_dir, "train")]
+            validation_directories = [os.path.join(data_dir, "validation")]
+        else:
+            train_directories = ["/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/oasis_125/train"]
+            validation_directories = ["/net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/oasis_125/validation"]
 
-    train_dataset = MRIDatasetNumpySlices(train_directories)
-    validation_dataset = MRIDatasetNumpySlices(validation_directories)
+    train_dataset = MRIDatasetNumpySlices(train_directories, t1_to_t2=config_train.T1_TO_T2)
+    validation_dataset = MRIDatasetNumpySlices(validation_directories, t1_to_t2=config_train.T1_TO_T2)
+    representative_test_dir = train_directories[0].split('/')[-2]
 
     num_workers = config_train.NUM_WORKERS
     print(f"Training with {num_workers} num_workers.")
@@ -35,7 +42,6 @@ if __name__ == '__main__':
                                  pin_memory=True)
         valloader = DataLoader(validation_dataset,
                                batch_size=config_train.BATCH_SIZE,
-                               shuffle=True,
                                num_workers=config_train.NUM_WORKERS,
                                pin_memory=True)
 
@@ -48,14 +54,14 @@ if __name__ == '__main__':
                            validationloader=valloader,
                            epochs=config_train.N_EPOCHS_CENTRALIZED,
                            filename="model.pth",
-                           model_dir=f"{config_train.DATA_ROOT_DIR}/trained_models/model-hgg_long-centralized-{config_train.LOSS_TYPE.name}-ep{config_train.N_EPOCHS_CENTRALIZED}-lr{config_train.LEARNING_RATE}-{config_train.NORMALIZATION.name}-{config_train.now.date()}-{config_train.now.hour}h",
+                           model_dir=f"{config_train.DATA_ROOT_DIR}/trained_models/model-{representative_test_dir}-{config_train.LOSS_TYPE.name}-ep{config_train.N_EPOCHS_CENTRALIZED}-lr{config_train.LEARNING_RATE}-{config_train.NORMALIZATION.name}-{config_train.now.date()}-{config_train.now.hour}h",
                            history_filename="history.pkl")
     else:
         unet.perform_train(trainloader, optimizer,
                            validationloader=valloader,
                            epochs=config_train.N_EPOCHS_CENTRALIZED,
                            filename="model.pth",
-                           model_dir=f"{config_train.DATA_ROOT_DIR}/trained_models/model-hgg_long-centralized-{config_train.LOSS_TYPE.name}-ep{config_train.N_EPOCHS_CENTRALIZED}-lr{config_train.LEARNING_RATE}-{config_train.NORMALIZATION.name}-{config_train.now.date()}-{config_train.now.hour}h",
+                           model_dir=f"{config_train.DATA_ROOT_DIR}/trained_models/model-{representative_test_dir}-{config_train.LOSS_TYPE.name}-ep{config_train.N_EPOCHS_CENTRALIZED}-lr{config_train.LEARNING_RATE}-{config_train.NORMALIZATION.name}-{config_train.now.date()}-{config_train.now.hour}h",
                            history_filename="history.pkl",
                            plots_dir="predictions",
                            save_best_model=True)
