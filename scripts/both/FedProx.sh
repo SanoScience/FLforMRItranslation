79a3ac329782904483dce9634a45693deeca8252
Jan Fiszer <plgfiszer@login01.athena.cyfronet.pl>
2024-03-25 15:14:46 +0100

    all scripts for every aggragation method

---
+++ scripts/both/FedProx.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+
+## Nazwa zlecenia
+#SBATCH -J FedProx
+# Liczba alokowanych węzłów
+#SBATCH -n 7
+## Liczba zadań per węzeł (domyślnie jest to liczba alokowanych rdzeni na węźle)
+#SBATCH --ntasks-per-node=1
+## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 5GB na rdzeń)
+##SBATCH --mem=100GB
+#SBATCH --mem-per-cpu=10GB
+## Maksymalny czas trwania zlecenia (format HH:MM:SS)
+#SBATCH --time=04:00:00
+## Nazwa grantu do rozliczenia zużycia zasobów
+#SBATCH -A plgfmri2-gpu-a100
+## Specyfikacja partycji
+#SBATCH --partition=plgrid-gpu-a100
+#SBATCH --gpus-per-task=1
+## Plik ze standardowym wyjściem
+#SBATCH --output="logs/training/t2_to_t1/FL/FedProxmu1/global.out"
+#SBATCH --error="logs/training/t2_to_t1/FL/FedProxmu1/global.out"
+
+DIR_NAME=logs/training/t2_to_t1/FL/FedProxmu1
+echo $DIR_NAME
+
+clients=("hgg_125" "oasis" "lgg" "ucsf_150" "hcp_mgh_masks" "hcp_wu_minn")
+
+PORT=8088
+
+## All the sruns are launched on the same node.
+## --exclusive ensures that when the server starts there is room for the clients 
+srun --ntasks=1 --cpus-per-task=1\
+    --output="./$DIR_NAME/server.out" --error="./$DIR_NAME/server_logs.out" \
+    $PLG_GROUPS_STORAGE/plggflmri/anaconda3/bin/python \
+    $HOME/repos/FLforMRItranslation/run_server.py $PORT fedprox &
+
+
+sleep 300
+
+for client in "${clients[@]}"; do
+    srun --ntasks=1 --cpus-per-task=1\
+    --output="./$DIR_NAME/$client.out" --error="./$DIR_NAME/error_$client.out" \
+    $PLG_GROUPS_STORAGE/plggflmri/anaconda3/bin/python $HOME/repos/FLforMRItranslation/run_client_train.py \
+    /net/pr2/projects/plgrid/plggflmri/Data/Internship/FL/$client $client $PORT fedprox &
+done
+wait
