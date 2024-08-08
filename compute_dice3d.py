bc4dd8894784f6c7740797a3a6e182c9c24344f0
Jan Fiszer <plgfiszer@login01.athena.cyfronet.pl>
2024-08-08 11:18:19 +0200

    FPR added

--- compute_dice3d.py
+++ compute_dice3d.py
@@ -1,11 +1,9 @@
 import os
 import sys
 import pickle
-import torch
-import importlib
 
-from configs import config_train, enums
-from src.ml import custom_metrics, datasets, models
+from configs import config_train
+from src.ml import custom_metrics, datasets
 from src.utils import visualization
 from torch.utils.data import DataLoader
 from torchmetrics.classification import Dice
@@ -16,10 +14,11 @@ if __name__ == '__main__':
     target_dir = sys.argv[1]
     predicted_dir = sys.argv[2]
 
-    eval_dataset = datasets.VoluminEvaluation(target_dir, predicted_dir)
+    eval_dataset = datasets.VolumeEvaluation(target_dir, predicted_dir)
     dataloader = DataLoader(eval_dataset, batch_size=1, shuffle=True)
 
     dice_scores = []
+    false_positives = []
 
     for batch_index, batch in enumerate(dataloader):
         targets_cpu, predicted_cpu = batch[0], batch[1]
@@ -27,8 +26,18 @@ if __name__ == '__main__':
         targets = targets_cpu.to(config_train.DEVICE)
         predicted = predicted_cpu.to(config_train.DEVICE)
 
-        dice_scores.append(dice(targets, predicted))
+        dice_scores.append(dice(predicted.cpu().int(), targets.cpu()))
+        false_positives.append(custom_metrics.false_positive_ratio(predicted.cpu().int(), targets.cpu().int()))
 
     average_dice = sum(dice_scores)/len(dice_scores)
+    false_positive_ratio = sum(false_positives)/len(false_positives)
+
     print(f"The 3D dice score is: {average_dice}")
+    print(f"The 3D false positive ratio is: {false_positive_ratio}")
+
+
+    filepath = os.path.join(predicted_dir, f"test_dice3d_{average_dice:.2f}.pkl")
+
+    with open(filepath, "wb") as file:
+        pickle.dump({"dice3d": average_dice, "fpr": false_positive_ratio}, file)
 
