617e1e388be581db6afcd4d38e6a4847f3f33a5b
JanFiszer <jf.janfiszer@gmail.com>
2024-12-04 15:57:01 +0100

    std calculation enabled

--- evaluate.py
+++ evaluate.py
@@ -13,7 +13,6 @@ from torch.utils.data import DataLoader
 import numpy as np
 
 
-
 class DifferentTranslationError(Exception):
     pass
 
@@ -31,21 +30,25 @@ if __name__ == '__main__':
     target_dir = None
     segmentation_task = False
     TRANSLATION = config_train.TRANSLATION
+    wanted_metrics = config_train.METRICS
     input_target_union = False
     squeeze = False
+    BATCH_SIZE = 1
 
     if config_train.LOCAL:
         test_dir = "C:\\Users\\JanFiszer\\data\\mri\\zoomed_ssim_test"
         model_path = "C:\\Users\\JanFiszer\\repos\\FLforMRItranslation\\trained_models\\fl\\flair_to_t2\\model-fedadam-MSE_DSSIM-FLAIRT2-lr0.001-rd32-ep4-2024-04-07\\round32.pth"
-        BATCH_SIZE = 1
+        representative_test_dir = "oasis"
     else:
         test_dir = sys.argv[1]
         model_path = sys.argv[2]
-        BATCH_SIZE = int(sys.argv[3])
-
+        # BATCH_SIZE = int(sys.argv[3])
+        # evaluate to have a
         if len(sys.argv) > 5:
             target_dir = sys.argv[5]
-            
+
+        representative_test_dir = test_dir.split(os.path.sep)[-2]
+
     if len(sys.argv) > 4:
         config_path = sys.argv[4]
     else:
@@ -53,7 +56,6 @@ if __name__ == '__main__':
 
     model_dir = '/'.join(e for e in model_path.split(os.path.sep)[:-1])
 
-    representative_test_dir = test_dir.split(os.path.sep)[-2]
     print("Model dir is: ", model_dir)
 
     if representative_test_dir in ["ucsf_150", "hgg_125", "lgg"]:
@@ -67,12 +69,13 @@ if __name__ == '__main__':
         TRANSLATION = imported_config.TRANSLATION
         print(f"\n\nTranslations: {imported_config.TRANSLATION}")
 
-        segmentation_task = imported_config.TRANSLATION[1] == enums.ImageModality.MASK or imported_config.TRANSLATION[1] == enums.ImageModality.TUMOR
+        segmentation_task = imported_config.TRANSLATION[1] == enums.ImageModality.MASK or imported_config.TRANSLATION[
+            1] == enums.ImageModality.TUMOR
         if segmentation_task:
             print("\nMask as the target modality, evaluation for segmenatation task\n")
 
         if imported_config.TRANSLATION[1] == enums.ImageModality.TUMOR:
-            input_target_union=True
+            input_target_union = True
             print("Taking `tumor` datasets, only union will be taken.")
 
     except FileNotFoundError:
@@ -82,15 +85,16 @@ if __name__ == '__main__':
         metric_mask_dir = "mask"
     else:
         metric_mask_dir = None
+        wanted_metrics.remove("zoomed_ssim")
 
-    testset = datasets.MRIDatasetNumpySlices(test_dir, 
+    testset = datasets.MRIDatasetNumpySlices(test_dir,
                                              target_dir=target_dir,
                                              translation_direction=TRANSLATION,
                                              binarize=segmentation_task,
                                              squeeze=squeeze,
                                              metric_mask_dir=metric_mask_dir,
                                              input_target_set_union=input_target_union)
-    
+
     testloader = DataLoader(testset, batch_size=BATCH_SIZE)
 
     if "prox" in model_path.lower():
@@ -100,7 +104,7 @@ if __name__ == '__main__':
         criterion = custom_metrics.BinaryDiceLoss()
     else:
         criterion = custom_metrics.DssimMse()
-    
+
     print(f"Taken criterion is: {criterion}")
     unet = models.UNet(criterion).to(config_train.DEVICE)
 
@@ -123,24 +127,25 @@ if __name__ == '__main__':
         predictions = unet(images)
 
         visualization.plot_batch([images.to('cpu'), targets.to('cpu'), predictions.to('cpu').detach()],
-                                filepath=os.path.join(test_dir, f"segmenation_results"))
+                                 filepath=os.path.join(test_dir, f"segmenation_results"))
 
     print(f"Model and data loaded; evaluation starts...")
-    if segmentation_task:
-        save_preds_dir = os.path.join(test_dir, "segmenatation")
-    else:
-        save_preds_dir = os.path.join(model_dir, "preds", representative_test_dir)
-    
-    wanted_metrics = ["ssim", "masked_ssim", "pnsr", "mse", "masked_mse", "relative_error", "zoomed_ssim"]
-    metrics = unet.evaluate(testloader, wanted_metrics=wanted_metrics, save_preds_dir=save_preds_dir, min_mask_pixel_in_batch=100)
-    
-    if segmentation_task:
-        filepath = os.path.join(model_dir, f"test_{representative_test_dir}_dice_{metrics['val_dice_2_class']:.2f}.pkl")
-    elif "zoomed_ssim" in wanted_metrics:
-        filepath = os.path.join(model_dir, f"test_{representative_test_dir}_zoomed_ssim_{metrics['val_zoomed_ssim']:.2f}.pkl")
-    else:
-        filepath = os.path.join(model_dir, f"test_{representative_test_dir}_ssim_{metrics['val_ssim']:.2f}.pkl")
 
-    with open(filepath, "wb") as file:
+    save_preds_dir = os.path.join(model_dir, "preds", representative_test_dir)
+
+    metrics, stds = unet.evaluate(testloader,
+                                  wanted_metrics=wanted_metrics,
+                                  save_preds_dir=save_preds_dir,
+                                  compute_std=True)
+
+    metric_filepath = os.path.join(model_dir, f"metrics_{representative_test_dir}_ssim_{metrics['val_ssim']:.2f}.pkl")
+    std_filepath = os.path.join(model_dir, f"std_{representative_test_dir}_ssim_{metrics['val_ssim']:.2f}.pkl")
+
+    with open(metric_filepath, "wb") as file:
         pickle.dump(metrics, file)
-    print(f"Saved to : {filepath}")
+    print(f"Metrics saved to : {metric_filepath}")
+
+    with open(std_filepath, "wb") as file:
+        pickle.dump(stds, file)
+    print(f"Standard deviations saved to : {std_filepath}")
+
